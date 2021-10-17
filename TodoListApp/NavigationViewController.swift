67fda666c61ec334affa517ffe24d704bc46ba5f
onomichi <83679529+Takata1124@users.noreply.github.com>
2021-10-18 02:31:16 +0900

    firstCommit

--- TodoListApp/NavigationViewController.swift
+++ TodoListApp/NavigationViewController.swift
@@ -11,6 +11,7 @@ class NavigationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ttt")
 
         // Do any additional setup after loading the view.
     }
