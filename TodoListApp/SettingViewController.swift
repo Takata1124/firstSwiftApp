994660fea484c2d3851dd7211982a590af37c6e4
onomichi <83679529+Takata1124@users.noreply.github.com>
2021-10-24 10:37:17 +0900

    subranch

--- TodoListApp/SettingViewController.swift
+++ TodoListApp/SettingViewController.swift
@@ -7,6 +7,33 @@
 
 import UIKit
 
+public protocol NSCoding {
+
+    func encode(with aCoder: NSCoder)
+    init?(coder aDecoder: NSCoder) // NS_DESIGNATED_INITIALIZER
+}
+
+var todoList: [TodoData] = []
+
+class TodoData: NSObject, NSCoding {
+    
+    var todoTitle: String?
+    var todoDone: Bool = false
+
+    
+    override init(){}
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(todoTitle, forKey: "todoTitle")
+        aCoder.encode(todoDone, forKey: "todoDone")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        todoTitle = aDecoder.decodeObject(forKey: "todoTitle") as? String
+        todoDone = aDecoder.decodeBool(forKey: "todoDone")
+    }
+}
+
 class SettingViewController: UIViewController, UITextFieldDelegate, UINavigationBarDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var newText: UITextField!
@@ -35,7 +62,21 @@ class SettingViewController: UIViewController, UITextFieldDelegate, UINavigation
             // 文字の色
                 .foregroundColor: UIColor.black
             ]
+        
+        if let storeData = UserDefaults().data(forKey: "todoList") {
+            do {
+                let unarchiveData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(storeData)
+                todoList.append(contentsOf: unarchiveData as! [TodoData])
+            } catch {
+                print(error)
+            }
+        }
     }
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+    }
+    
     
     @IBAction func backbutton(_ sender: Any) {
 
